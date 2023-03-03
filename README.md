09432e7f474cba9fc86f89f39066f8429415e300
Błażej Szczygieł <spaz16@wp.pl>
2023-03-03 21:25:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# xfce4-dpr-changer
+
+## Description
+
+This tool allows to change display scaling from system tray in Xfce4 desktop
+
+## Installation (Linux)
+
+```sh
+git clone https://github.com/zaps166/xfce4-dpr-changer.git
+cd xfce4-dpr-changer
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr
+make
+sudo make install/strip
+```
+
+---
+
+It's available in [AUR](https://aur.archlinux.org/packages/xfce4-dpr-changer-git)
---
+++ main.cpp
@@ -0,0 +1,240 @@
+#include <QCommandLineParser>
+#include <QSystemTrayIcon>
+#include <QActionGroup>
+#include <QApplication>
+#include <QDirIterator>
+#include <QProcess>
+#include <QDebug>
+#include <QMenu>
+
+#include <QDBusPendingCall>
+#include <QDBusConnection>
+#include <QDBusMessage>
+
+using namespace std;
+
+constexpr auto g_windowScalingFactorPath = "/Gdk/WindowScalingFactor";
+constexpr auto g_unscaledDpiPath = "/Gdk/UnscaledDPI";
+constexpr auto g_dpiPath = "/Xft/DPI";
+
+constexpr auto g_xfwm4ThemePath = "/general/theme";
+
+constexpr auto g_dpiReference = 96;
+
+static unique_ptr<QProcess> getXsettingValueAsync(const QString &path)
+{
+    auto process = make_unique<QProcess>();
+    process->start("xfconf-query", {"-c", "xsettings", "-p", path});
+    return process;
+}
+static int getIntValueFromAsyncProcess(const unique_ptr<QProcess> &process)
+{
+    if (!process->waitForFinished())
+        return 0;
+    if (process->exitCode() != 0)
+        return 0;
+    return process->readAllStandardOutput().toInt();
+}
+
+static QString getXfwm4Theme()
+{
+    QProcess process;
+    process.start("xfconf-query", {"-c", "xfwm4", "-p", g_xfwm4ThemePath});
+    if (!process.waitForFinished())
+        return QString();
+    if (process.exitCode() != 0)
+        return QString();
+    return process.readAllStandardOutput().trimmed();
+}
+static void maybeSetXfwm4Theme(const QString &theme)
+{
+    QDirIterator allThemesIt(DATA_DIR "/themes", QDir::Dirs | QDir::NoDotAndDotDot);
+    while (allThemesIt.hasNext())
+    {
+        const auto themeDir = allThemesIt.nextFileInfo();
+        QDirIterator themeIt(themeDir.filePath(), QDir::Dirs | QDir::NoDotAndDotDot);
+        while (themeIt.hasNext())
+        {
+            if (themeIt.nextFileInfo().fileName() == QStringLiteral("xfwm4") && themeDir.fileName() == theme)
+            {
+                QProcess::startDetached("xfconf-query", {"-c", "xfwm4", "-p", g_xfwm4ThemePath, "-s", theme});
+                break;
+            }
+        }
+    }
+}
+
+static void setXsettingIntValue(const QString &path, int value)
+{
+    QProcess::startDetached("xfconf-query", {"-c", "xsettings", "-n", "-p", path, "-t", "int", "-s", QString::number(value)});
+}
+static void resetXsettingIntValue(const QString &path)
+{
+    QProcess::startDetached("xfconf-query", {"-c", "xsettings", "-p", path, "-r"});
+}
+
+static inline int getScale(int dpi)
+{
+    return qRound(dpi * 100.0 / g_dpiReference);
+}
+
+static inline int getDpi(int scale)
+{
+    return qRound(g_dpiReference * scale / 100.0);
+}
+static inline int getWindowScalingFactor(int scale)
+{
+    return qRound(scale / 100.0);
+}
+static inline int getUnscaledDpi(int dpi, int windowScalingFactor)
+{
+    return qRound(dpi * 1000.0 / windowScalingFactor);
+}
+
+static int getCurrentScale()
+{
+    const auto currDpiProcess = getXsettingValueAsync(g_dpiPath);
+    const auto currWindowScalingFactorProcess = getXsettingValueAsync(g_windowScalingFactorPath);
+    const auto currUnscaledDpiProcess = getXsettingValueAsync(g_unscaledDpiPath);
+
+    const int currDpi = getIntValueFromAsyncProcess(currDpiProcess);
+    const int currWindowScalingFactor = getIntValueFromAsyncProcess(currWindowScalingFactorProcess);
+    const int currUnscaledDpi = getIntValueFromAsyncProcess(currUnscaledDpiProcess);
+
+    if (currDpi > 0 && currWindowScalingFactor > 0)
+    {
+        const int scale = getScale(currDpi);
+        const int windowScalingFactor = getWindowScalingFactor(scale);
+        const int unscaledDpi = getUnscaledDpi(currDpi, windowScalingFactor);
+        if (windowScalingFactor == currWindowScalingFactor && ((currUnscaledDpi == 0 && unscaledDpi == g_dpiReference * 1000) || (currUnscaledDpi > 0 && unscaledDpi == currUnscaledDpi)))
+            return scale;
+    }
+
+    return 0;
+}
+
+static void scalingActionTriggered(int scale)
+{
+    const int dpi = getDpi(scale);
+    const int windowScalingFactor = getWindowScalingFactor(scale);
+    const int unscaledDpi = getUnscaledDpi(dpi, windowScalingFactor);
+
+    setXsettingIntValue(g_windowScalingFactorPath, windowScalingFactor);
+    if (unscaledDpi == g_dpiReference * 1000)
+        resetXsettingIntValue(g_unscaledDpiPath);
+    else
+        setXsettingIntValue(g_unscaledDpiPath, unscaledDpi);
+    setXsettingIntValue(g_dpiPath, dpi);
+    setXsettingIntValue("/Xfce/LastCustomDPI", dpi);
+
+    // Restart Xfce4 panel
+    if (auto sessionBus = QDBusConnection::sessionBus(); sessionBus.isConnected())
+    {
+        sessionBus.asyncCall(
+            QDBusMessage::createMethodCall(
+                "org.xfce.Panel",
+                "/org/xfce/Panel",
+                "org.xfce.Panel",
+                "Terminate"
+            ) << true // Restart (argument)
+        );
+    }
+
+    // Set Xfwm4 style
+    if (const auto theme = getXfwm4Theme(); !theme.isEmpty())
+    {
+        auto newTheme = theme;
+        newTheme.remove(QStringLiteral("-hdpi"));
+        newTheme.remove(QStringLiteral("-xhdpi"));
+        if (windowScalingFactor == 2)
+            newTheme.append(QStringLiteral("-hdpi"));
+        else if (windowScalingFactor > 2)
+            newTheme.append(QStringLiteral("-xhdpi"));
+        if (theme != newTheme)
+            maybeSetXfwm4Theme(newTheme);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    qInstallMessageHandler([](QtMsgType t, const QMessageLogContext &c, const QString &s) {
+        fprintf(stderr, "%s\n", qUtf8Printable(qFormatLogMessage(t, c, s)));
+        fflush(stderr);
+    });
+
+    QApplication app(argc, argv);
+    app.setApplicationName("Display scaling");
+
+    QCommandLineParser parser;
+    parser.addPositionalArgument("scale", "Set scaling");
+    parser.addHelpOption();
+    parser.process(app);
+    if (const auto positionalArgs = parser.positionalArguments(); positionalArgs.size() == 1)
+    {
+        const int scale = positionalArgs[0].toInt();
+        if (scale >= 100 && scale <= 300)
+        {
+            scalingActionTriggered(scale);
+            return 0;
+        }
+    }
+
+    QMenu menu;
+    QActionGroup scalingGroup(&menu);
+
+    bool blockToggled = false;
+
+    QObject::connect(&menu, &QMenu::aboutToShow, [&menu, &blockToggled] {
+        const int currentScale = getCurrentScale();
+        if (currentScale <= 0)
+            return;
+
+        const auto actions = menu.actions();
+        for (QAction *act : actions)
+        {
+            const int scale = act->data().toInt();
+            if (scale == currentScale)
+            {
+                blockToggled = true;
+                act->setChecked(true);
+                blockToggled = false;
+            }
+        }
+    });
+
+    const int scales[] {
+        100,
+        125,
+        170,
+        200,
+        300,
+    };
+    for (int scale : scales)
+    {
+        auto act = menu.addAction(QString("%1%").arg(scale));
+        act->setCheckable(true);
+        act->setData(scale);
+        scalingGroup.addAction(act);
+        QObject::connect(act, &QAction::toggled, [scale, &blockToggled](bool checked) {
+            if (!blockToggled && checked)
+                scalingActionTriggered(scale);
+        });
+    }
+
+    QProcess dispSettings;
+    dispSettings.setProgram("xfce4-display-settings");
+
+    QSystemTrayIcon tray;
+    tray.setIcon(QIcon::fromTheme("preferences-desktop-display"));
+    tray.setContextMenu(&menu);
+    QObject::connect(&tray, &QSystemTrayIcon::activated, [&dispSettings](QSystemTrayIcon::ActivationReason reason) {
+        if (reason == QSystemTrayIcon::Trigger)
+        {
+            if (dispSettings.state() == QProcess::NotRunning)
+                dispSettings.start();
+        }
+    });
+    tray.show();
+
+    return app.exec();
+}
